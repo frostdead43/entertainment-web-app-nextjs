c69ff62cb1df63dede535a93a18e2fbdcff78471
frostdead43 <frostdead@hotmail.com>
2025-08-09 21:12:56 +0300

    css styles are added and new fetch created

--- lib/fetchers.jsx
+++ lib/fetchers.jsx
@@ -1,10 +1,20 @@
 "use server"
 export async function fetchPopularMovies() {
   const res = await fetch(
-    `https://api.themoviedb.org/3/tv/popular?api_key=${process.env.TMDB_API_KEY}`
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}`
   );
 
   const data = await res.json();
-  console.log(data);
+  // console.log(data);
   return data.results;
+}
+
+export async function fetchPopularTv() {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/tv/popular?api_key=${process.env.TMDB_API_KEY}`
+  );
+
+  const tvdata = await res.json();
+  // console.log(tvdata);
+  return tvdata.results;
 }
